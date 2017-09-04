3546f7fef8f08885b27b3236327f70c41a1cb628
silent <silent161192@gmail.com>
2017-09-04 06:49:20 +0300

    начинаю изучать

--- spec.c
+++ spec.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main
+int main()
+{
+    int age, weight;
+    char name[50];
+    printf("Введите имя: ");
+    scanf("%s", name);
+    printf("Возраст: ");
+    scanf("%d", &age);
+    printf("Введите вес: ");
+    scanf("%d", &weight);
+    printf("Вывод: \n");
+    printf("Ваше имя %s, ваш возраст %d, ваш вес - %d\n", name, age, weight);
+    printf("Адрес переменной - 0x%p\n", &name);
+}
